89d5f579daa678e0536a4bca52ba78c94814d5e6
Elankumaran Kuppusamy <rk.elankumaran@gmail.com>
2017-02-28 22:54:52 -0600

    added address change

--- messages/index.js
+++ messages/index.js
@@ -10,6 +10,7 @@ var botbuilder_azure = require("botbuilder-azure");
 
 var useEmulator = (process.env.NODE_ENV == 'development');
 
+
 var connector = useEmulator ? new builder.ChatConnector() : new botbuilder_azure.BotServiceConnector({
     appId: process.env['MicrosoftAppId'],
     appPassword: process.env['MicrosoftAppPassword'],
@@ -24,20 +25,26 @@ var luisAppId = process.env.LuisAppId;
 var luisAPIKey = process.env.LuisAPIKey;
 var luisAPIHostName = process.env.LuisAPIHostName || 'api.projectoxford.ai';
 
-const LuisModelUrl = 'https://' + luisAPIHostName + '/luis/v1/application?id=' + luisAppId + '&subscription-key=' + luisAPIKey;
+const LuisModelUrl = 'https://' + luisAPIHostName + '/luis/v2.0/apps/' + luisAppId + '?subscription-key=' + luisAPIKey;
+
 
 // Main dialog with LUIS
 var recognizer = new builder.LuisRecognizer(LuisModelUrl);
 var intents = new builder.IntentDialog({ recognizers: [recognizer] })
-/*
-.matches('<yourIntent>')... See details at http://docs.botframework.com/builder/node/guides/understanding-natural-language/
-*/
 .matches('None', (session, args) => {
     session.send('Hi! This is the None intent handler. You said: \'%s\'.' + luisAppId, session.message.text);
 })
 .matches('greeting', (session, args) => {
     session.send('Hello there!');
 })
+.matches('address-change', [
+    function (session) {
+        session.beginDialog('/addresschange');
+    },
+    function (session, results) {
+        session.send('Ok... done!');
+    }
+])
 .matches('weather', (session, args) => {
     session.send('Hmmm weather data has not been hooked up yet!');
 })
@@ -45,7 +52,17 @@ var intents = new builder.IntentDialog({ recognizers: [recognizer] })
     session.send('Sorry, I did not understand \'%s\'.', session.message.text);
 });
 
-bot.dialog('/', intents);    
+bot.dialog('/', intents);   
+
+bot.dialog('/addresschange', [
+    function(session, args, next) {
+        builder.Prompts.text(session, 'Sure address change it is..whose address you would like to change?');
+    },
+    function(session, results) {
+        session.endDialog();
+    }
+
+]) 
 
 if (useEmulator) {
     var restify = require('restify');
